b01743ff2b94c52fc787b449362c6c30774bc806
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-12-09 15:52:26 +0100

    handle the cards textures loading into a dedicated module

--- rust-blackjack-client/src/main.rs
+++ rust-blackjack-client/src/main.rs
@@ -1,15 +1,13 @@
 extern crate piston_window;
 extern crate ws;
 
+mod cards;
+
 use piston_window::{
     clear,
     image,
     PistonWindow,
     WindowSettings,
-    Texture,
-    Flip,
-    TextureSettings,
-    G2dTexture,
     Transformed,
 };
 
@@ -69,42 +67,6 @@ impl Handler for Client {
     }
 }
 
-/// Loads one card texture. Refactored as called multiple times.
-///
-/// # Args:
-///
-/// `window` - piston window reference for textures loading
-/// `file_name` - name of the texture file to load (without prefix/suffix)
-fn load_one_card_texture(
-    window: &mut PistonWindow,
-    file_name: &str
-) -> G2dTexture {
-
-    let mut file_path: String = "res/".to_string();
-    file_path.push_str(file_name);
-    file_path.push_str(".png");
-
-    Texture::from_path(
-        &mut window.factory,
-        &file_path,
-        Flip::None,
-        &TextureSettings::new(),
-    ).unwrap()
-}
-
-/// Loads all cards textures into an array. Refactored for readability.
-///
-/// # Args:
-///
-/// `window` - piston window reference for textures loading
-fn load_all_cards_textures(window: &mut PistonWindow) -> Vec<G2dTexture> {
-
-    vec![
-        load_one_card_texture(window, "2_of_clubs"),
-        load_one_card_texture(window, "3_of_clubs"),
-    ]
-}
-
 fn main() {
 
     /* FIXME: for now, a player only has one unique card
@@ -148,7 +110,7 @@ fn main() {
     .build()
     .unwrap();
 
-    let cards = load_all_cards_textures(&mut window);
+    let cards = cards::load_all_cards_textures(&mut window);
 
     while let Some(event) = window.next() {
 
